63166a3a7496529ac414d567c43a583a7fbf3acb
Daedo1 <david.oh1606@gmail.com>
2025-06-26 18:27:51 -0700

    Create new Menu class and added getWindow method

---
+++ README.md
@@ -0,0 +1,5 @@
+Hi 
+
+this is a rogue like video game to-be-developed. Made
+in java swing. 
+
--- src/Window/Window.java
+++ src/Window/Window.java
@@ -9,11 +9,26 @@ public class Window extends JFrame {
     private final String TITLE = "RogueLike";
 
 
-    private static Window window;
+    private static Window window = null;
 
-    public Window() {
+    protected Window() {
+        
+        setTitle(TITLE);
+        setResizable(false);
 
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
+
+    public Window getWindow() {
+        if (Window.window == null) {
+            throw new IllegalStateException("Window hasn't been initialized");
+        }
+        
+        return window;
+    }
+
 
 
 }
