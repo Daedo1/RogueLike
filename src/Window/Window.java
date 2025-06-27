7920f2315741131324692b8583efdfbe79dc4dfd
Daedo1 <david.oh1606@gmail.com>
2025-06-26 18:00:23 -0700

    first commit

---
+++ src/Window/Window.java
@@ -0,0 +1,19 @@
+package Window;
+
+import java.awt.*;
+import javax.swing.*;
+
+
+public class Window extends JFrame {
+    
+    private final String TITLE = "RogueLike";
+
+
+    private static Window window;
+
+    public Window() {
+
+    }
+
+
+}
